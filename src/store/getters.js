957ac2fe8e9fa3ecf936c908909b88c4420264f7
NilBrains <167288520@qq.com>
2021-07-27 22:01:29 +0800

    clear main

--- src/store/getters.js
+++
@@ -1,8 +0,0 @@
-const getters = {
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name,
-  islogin: state => state.user.islogin,
-  uid:  state =>  state.user.uid
-}
-export default getters
